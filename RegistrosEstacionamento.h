f05d5223de9cf6f967c943adcfe928dc70ef369e
goncalohvber <goncalohvber@gmail.com>
2026-01-02 23:30:40 +0000

    Delete RegistrosEstacionamento.h

--- RegistrosEstacionamento.h
+++
@@ -1,9 +0,0 @@
-#ifndef RegistrosEstacionamento_h
-#define RegistrosEstacionamento_h
-
-#include "Instalacao.h"
-
-void atualizarValidacao(Confparque config);
-void ProcessarLugar(char *lugarStr, int *piso, int *fila, int *numero);
-
-#endif /* RegistrosEstacionamento_h */
