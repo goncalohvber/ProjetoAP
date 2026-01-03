c4b79e3847186344e640d57f5ef0b5e275469e10
goncalohvber <goncalohvber@gmail.com>
2026-01-03 23:39:20 +0000

    COMPLETO

--- RegistrosEstacionamento.c
+++ RegistrosEstacionamento.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 void LimpaFicheiroEstac(char *ficheirobase, char *ficheirovalido, char *ficheiroerros, Confparque config);
 
+// CHAMA NOVAMENTE A FUNCAO LIMPAR PARA VALIDAR OS ESTACIONAMENTOS.
 void atualizarValidacao(Confparque config) {
     printf("\n Atualizando ficheiro validado...\n");
 
